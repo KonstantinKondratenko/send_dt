238848f1788d367a683149f8dbd2d161dc33f644
Konstantin Evgenievich Kondratenko <90711883+KonstantinKondratenko@users.noreply.github.com>
2024-04-26 16:58:01 +0300

    Update move.py

--- move.py
+++ move.py
@@ -3,17 +3,13 @@ import numpy as np
 current_position = np.array([x1, y1])
 target_position = np.array([x2, y2])
 displacement_vector = target_position - current_position
-
-
 heading_angle_rad = np.radians(heading_angle)
 heading_vector = np.array([np.cos(heading_angle_rad), np.sin(heading_angle_rad)])
+angle_between_vectors = np.arccos(np.dot(displacement_vector, heading_vector) / (np.linalg.norm(displacement_vector) * np.linalg.norm(heading_vector)))
+distance_to_target = np.linalg.norm(displacement_vector)
 
+time_interval = some_time_interval  
 
-tangential_component = np.dot(displacement_vector, heading_vector) * heading_vector
+tangential_acceleration = (distance_to_target / time_interval ** 2) * np.cos(angle_between_vectors)
 
-
-normal_vector = np.array([-heading_vector[1], heading_vector[0]])
-normal_component = np.dot(displacement_vector, normal_vector) * normal_vector
-
-tangential_acceleration = tangential_component / some_time_interval
-normal_acceleration = normal_component / some_time_interval
+angular_acceleration = (angle_between_vectors / time_interval ** 2) * np.sign(np.cross(displacement_vector, heading_vector))
